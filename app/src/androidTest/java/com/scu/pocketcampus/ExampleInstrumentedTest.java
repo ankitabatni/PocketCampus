19f028b741694fd64419d8cba0b83e0a6a9ea457
Ankita Batni <ankitabatni@gmail.com>
2017-06-14 10:31:20 -0700

    Changing the package name in remaining files.

--- app/src/androidTest/java/com/scu/pocketcampus/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/scu/pocketcampus/ExampleInstrumentedTest.java
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
 
-        assertEquals("com.example.ankita.onestopcampus", appContext.getPackageName());
+        assertEquals("com.scu.pocketcampus", appContext.getPackageName());
     }
 }
