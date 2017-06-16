51dbb678f1009946eea61e22eb7719ed27dbf67d
Ankita Batni <ankitabatni@gmail.com>
2017-06-16 13:13:42 -0700

    Home Screen Changes

--- app/src/main/java/com/scu/pocketcampus/HomeScreen.java
+++ app/src/main/java/com/scu/pocketcampus/HomeScreen.java
@@ -2,10 +2,14 @@ package com.scu.pocketcampus;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.ViewFlipper;
 
 import com.google.firebase.auth.FirebaseAuth;
 
@@ -16,13 +20,20 @@ import com.google.firebase.auth.FirebaseAuth;
     public class HomeScreen extends AppCompatActivity {
     private Menu mainMenu;
     private FirebaseAuth firebaseAuth;
-
+    private ViewFlipper myViewFlipper;
+    private float initialXPoint;
+    int[] image = { R.drawable.one_full, R.drawable.two_full,
+            R.drawable.three_full, R.drawable.four_full, R.drawable.five_full,
+            R.drawable.six_full, R.drawable.seven_full, R.drawable.eight_full,
+            R.drawable.nine_full, R.drawable.ten_full ,R.drawable.eleven_full, R.drawable.twelve_full };
+    DrawerLayout mDrawerLayout;
+    ListView mDrawerList;
         //private static Button button3_sbm, button4_sbm;
 
 
         @Override
         public void onBackPressed() {
-            
+
         }
 
         @Override
@@ -30,7 +41,16 @@ import com.google.firebase.auth.FirebaseAuth;
             super.onCreate(savedInstanceState);
             setContentView(R.layout.activity_homescreen);
             setTitle("Pocket Campus");
-            //OnClickButtonListener();
+            myViewFlipper = (ViewFlipper) findViewById(R.id.myflipper);
+
+            for (int i = 0; i < image.length; i++) {
+                ImageView imageView = new ImageView(HomeScreen.this);
+                imageView.setImageResource(image[i]);
+                myViewFlipper.addView(imageView);
+            }
+            myViewFlipper.setAutoStart(true);
+            myViewFlipper.setFlipInterval(2000);
+            myViewFlipper.startFlipping();
         }
 
         public void onClick(View v) {
@@ -45,12 +65,12 @@ import com.google.firebase.auth.FirebaseAuth;
                     if (v.getId() == R.id.button_chat) {
                     Intent intent = new Intent(this, Chat.class);
                     startActivity(intent);
-            }
-            if(v.getId()==R.id.buttonLocate)
-            {
-                Intent i2=new Intent(this,Location.class);
-                startActivity(i2);
-            }
+                    }
+                    if(v.getId()==R.id.buttonLocate)
+                    {
+                        Intent i2=new Intent(this,Location.class);
+                        startActivity(i2);
+                    }
                 }
 
     @Override
